a3492226908ada9c575edab6d56dae4b443fb699
Tom <thomas.spencer@codurance.com>
2020-06-13 00:14:01 +0100

    Disproving test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,10 @@ describe 'FizzBuzz' do
       expect(is_divisible_by_three?(3)).to eq(true)
     end
   end
+
+  context 'knows that a number is not divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(3)).not_to eq(true)
+    end
+  end
 end
