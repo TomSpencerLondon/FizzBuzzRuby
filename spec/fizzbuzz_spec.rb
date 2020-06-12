fcabc4a7b3db1c7b443ac40b6cd72d0153b84274
Tom <thomas.spencer@codurance.com>
2020-06-13 00:16:14 +0100

    Disprove divisible 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,11 +9,18 @@ describe 'FizzBuzz' do
     it '3' do
       expect(is_divisible_by_three?(3)).to eq(true)
     end
+
+    it '5' do
+      expect(is_divisible_by_five?(5)).to eq(true)
+    end
   end
 
   context 'knows that a number is not divisible by' do
     it '3' do
       expect(is_divisible_by_three?(1)).not_to eq(true)
+    end
+    it '5' do
+      expect(is_divisible_by_five?(1)).not_to eq(true)
     end
   end
 end
