2ba4fb9cd1cf0c27adb54467149d9a58e60a0595
Tom <thomas.spencer@codurance.com>
2020-06-13 11:06:47 +0100

    Add rubocop corrections

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-
 def fizzbuzz(number)
-  return 'FizzBuzz' if is_divisible_by_fifteen?(number)
-  return 'Buzz' if is_divisible_by_five?(number)
-  return 'Fizz' if is_divisible_by_three?(number)
+  return 'FizzBuzz' if divisible_by_fifteen?(number)
+  return 'Buzz' if divisible_by_five?(number)
+  return 'Fizz' if divisible_by_three?(number)
+
   number
 end
 
-def is_divisible_by_three?(number)
-  is_divisible_by?(number, 3)
+def divisible_by_three?(number)
+  divisible_by?(number, 3)
 end
 
-def is_divisible_by_five?(number)
-  is_divisible_by?(number, 5)
+def divisible_by_five?(number)
+  divisible_by?(number, 5)
 end
 
-def is_divisible_by_fifteen?(number)
-  is_divisible_by?(number, 15)
+def divisible_by_fifteen?(number)
+  divisible_by?(number, 15)
 end
 
-def is_divisible_by?(number, divisor)
-  number % divisor == 0
+def divisible_by?(number, divisor)
+  (number % divisor).zero?
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'fizzbuzz'
 
 # Write a program that prints the numbers from 1 to 100.
@@ -7,28 +9,28 @@ require 'fizzbuzz'
 describe 'FizzBuzz' do
   context  'knows that a number is divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(3)).to eq true
+      expect(divisible_by_three?(3)).to eq true
     end
 
     it '5' do
-      expect(is_divisible_by_five?(5)).to eq true
+      expect(divisible_by_five?(5)).to eq true
     end
 
     it '15' do
-      expect(is_divisible_by_fifteen?(15)).to eq true
+      expect(divisible_by_fifteen?(15)).to eq true
     end
   end
 
   context 'knows that a number is not divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(1)).not_to eq true
+      expect(divisible_by_three?(1)).not_to eq true
     end
     it '5' do
-      expect(is_divisible_by_five?(1)).not_to eq true
+      expect(divisible_by_five?(1)).not_to eq true
     end
 
     it '15' do
-      expect(is_divisible_by_fifteen?(1)).not_to eq true
+      expect(divisible_by_fifteen?(1)).not_to eq true
     end
   end
 
